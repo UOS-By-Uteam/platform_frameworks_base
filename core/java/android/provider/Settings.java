df0009f758b61ee84920eda5f7557d6cb8487cd8
htchoi1995 <htchoi1995@gmail.com>
2016-01-03 20:26:36 +0100

    FWB: Disable/Enable screenshot sound [1/2]
    Change-Id: I9f32d0026833f9350a2e8e4bedaff3801f508e76
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3695,6 +3695,12 @@ public final class Settings {
          public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
 
         /**
+         * Enable/Disable screenshot sound
+         * @hide
+         */
+        public static final String SCREENSHOT_SOUND = "screenshot_sound";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
