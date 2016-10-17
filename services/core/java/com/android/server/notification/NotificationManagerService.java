c457a671244a979589fd1cb57746f8e673c11654
Danny Baumann <dannybaumann@web.de>
2016-10-17 16:39:46 +0200

    (Optionally) allow vibration during priority zen mode.
    Change-Id: I6fb81c5898fbfe9e89a4af3fdc042266c8a9be6c
    
    Conflicts:
    	services/core/java/com/android/server/notification/NotificationManagerService.java

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -1189,6 +1189,7 @@ public class NotificationManagerService extends SystemService {
         }
         mZenModeHelper.initZenMode();
         mZenModeHelper.readAllowLightsFromSettings();
+        mZenModeHelper.readVibrationModeFromSettings();
         mInterruptionFilter = mZenModeHelper.getZenModeListenerInterruptionFilter();
 
         mUserProfiles.updateCache(getContext());
@@ -2215,7 +2216,7 @@ public class NotificationManagerService extends SystemService {
         }
 
         @Override
-        public boolean matchesCallFilter(Bundle extras) {
+        public boolean[] matchesCallFilter(Bundle extras) {
             enforceSystemOrSystemUI("INotificationManager.matchesCallFilter");
             return mZenModeHelper.matchesCallFilter(
                     Binder.getCallingUserHandle(),
@@ -3011,14 +3012,19 @@ public class NotificationManagerService extends SystemService {
         boolean hasValidVibrate = false;
         boolean hasValidSound = false;
 
-        if (disableEffects == null
+        boolean readyForBeepOrBuzz = disableEffects == null
                 && (record.getUserId() == UserHandle.USER_ALL ||
                     record.getUserId() == currentUser ||
                     mUserProfiles.isCurrentProfile(record.getUserId()))
-                && canInterrupt
                 && mSystemReady
-                && !notificationIsAnnoying(pkg)
-                && mAudioManager != null) {
+                && mAudioManager != null;
+
+        boolean canBeep = readyForBeepOrBuzz && canInterrupt;
+        boolean canBuzz = readyForBeepOrBuzz &&
+                (canInterrupt ||
+                        (aboveThreshold && mZenModeHelper.allowVibrationForNotifications()));
+
+        if (canBeep || canBuzz) {
             if (DBG) Slog.v(TAG, "Interrupting!");
 
             // should we use the default notification sound? (indicated either by
@@ -3064,7 +3070,7 @@ public class NotificationManagerService extends SystemService {
 
                 sendAccessibilityEvent(notification, record.sbn.getPackageName());
 
-                if (hasValidSound) {
+                if (canBeep && hasValidSound) {
                     boolean looping =
                             (notification.flags & Notification.FLAG_INSISTENT) != 0;
                     AudioAttributes audioAttributes = audioAttributesForNotification(notification);
@@ -3092,8 +3098,8 @@ public class NotificationManagerService extends SystemService {
                     }
                 }
 
-                if (hasValidVibrate && !(mAudioManager.getRingerModeInternal()
-                        == AudioManager.RINGER_MODE_SILENT)) {
+                if (canBuzz && hasValidVibrate &&
+                        mAudioManager.getRingerModeInternal() != AudioManager.RINGER_MODE_SILENT) {
                     mVibrateNotificationKey = key;
 
                     if (useDefaultVibrate || convertSoundToVibration) {
